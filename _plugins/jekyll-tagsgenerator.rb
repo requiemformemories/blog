7f9e4f368dbd7c1f0332953c9dad3956fa233aa0
fumitsuki <fumitsuki@5xruby.tw>
2021-03-11 20:51:00 +0800

    fix: remove broken links

--- _plugins/jekyll-tagsgenerator.rb
+++ _plugins/jekyll-tagsgenerator.rb
@@ -1,3 +1,4 @@
+require 'slugify'
 module Jekyll
 
   class TagsGenerator < Generator
@@ -12,14 +13,21 @@ module Jekyll
 
     def build_subpages(site, type, posts)
       posts[1] = posts[1].sort_by { |p| -p.date.to_f }
+      atomize(site, type, posts)
       paginate(site, type, posts)
+    end
+
+    def atomize(site, type, posts)
+      path = "/tag/" + posts[0].slugify
+      atom = AtomPageTags.new(site, site.source, path, type, posts[0], posts[1])
+      site.pages << atom
     end
 
     def paginate(site, type, posts)
       pages = Jekyll::Paginate::Pager.calculate_pages(posts[1], site.config['paginate'].to_i)
       (1..pages).each do |num_page|
         pager = Jekyll::Paginate::Pager.new(site, num_page, posts[1], pages)
-        path = "/tag/#{posts[0]}".gsub(' ', '-').downcase
+        path = "/tag/" + posts[0].slugify
         if num_page > 1
           path = path + "/page#{num_page}"
         end
@@ -42,6 +50,21 @@ module Jekyll
       self.read_yaml(File.join(base, '_layouts'), "tag.html")
       self.data["grouptype"] = type
       self.data[type] = val
+    end
+  end
+
+  class AtomPageTags < Page
+    def initialize(site, base, dir, type, val, posts)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = 'feed.xml'
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), "feed.xml")
+      self.data[type] = val
+      self.data["grouptype"] = type
+      self.data["posts"] = posts[0..9]
     end
   end
 end
--- about/index.md
+++ about/index.md
@@ -1,6 +1,6 @@
 ---
 layout: page
-current: about 
+current: about
 title: About the Box
 navigation: true
 logo: 'assets/images/no-logo.jpg'
@@ -10,9 +10,3 @@ subclass: 'post page'
 
 文月的神奇雜貨箱，是文月用來放備份筆記的地方。
 希望這個破破爛爛的百寶箱，你也能在裡面找到一點有用或有趣的小東西呢OwO
-
-你大概可以在這個箱子裡找到這些東西：
-<ul>
-	<li><a href='{{ site.baseurl }}tag/rails/'>Rails 相關筆記</a></li>
-	<li><a href='{{ site.baseurl }}tag/5xruby/'>五倍紅寶石實習相關</a></li>
-</ul>
